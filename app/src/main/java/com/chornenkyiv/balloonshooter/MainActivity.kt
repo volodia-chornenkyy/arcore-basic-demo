1b64c5bcf63577080a3fb976edc78b946d619e0c
Volodia Chornenkyy <volodia.chornenkyy@techmagic.co>
2019-08-02 20:09:28 +0300

    Check for Camera Permission

--- app/src/main/java/com/chornenkyiv/balloonshooter/MainActivity.kt
+++ app/src/main/java/com/chornenkyiv/balloonshooter/MainActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.chornenkyiv.balloonshooter.ar.ARCoreAvailabilityCallback
 import com.chornenkyiv.balloonshooter.ar.ARCoreHelper
+import com.chornenkyiv.balloonshooter.ar.CameraPermissionHelper
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -29,5 +30,31 @@ class MainActivity : AppCompatActivity() {
                     .show()
             }
         })
+
+        btnEnterAr.setOnClickListener {
+            // ARCore requires camera permission to operate.
+            if (!CameraPermissionHelper.hasCameraPermission(this@MainActivity)) {
+                CameraPermissionHelper.requestCameraPermission(this@MainActivity)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        results: IntArray
+    ) {
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            Toast.makeText(
+                this,
+                "Camera permission is needed to run this application",
+                Toast.LENGTH_LONG
+            )
+                .show()
+            if (!CameraPermissionHelper.shouldShowRequestPermissionRationale(this)) {
+                // Permission denied with checking "Do not ask again".
+                CameraPermissionHelper.launchPermissionSettings(this)
+            }
+        }
     }
 }
